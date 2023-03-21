de72ffd4c10622bf4645f4d3837bee61fb875be9
Mitchel <mitchel.armani@gmail.com>
2023-03-21 13:12:38 -0700

    printf function in C language

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h> /*header file*/
+/**
+  * main - stands for printed format
+  * It prints characters to the screen
+  * given inside the double quote
+  * ends with a semi colon similar to puts
+  * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
