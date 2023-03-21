6267cce7647d83ce340848ef5c4ed3f6059f55f7
Mitchel <mitchel.armani@gmail.com>
2023-03-21 12:52:27 -0700

    Puts function in C language

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - write a line or string to the output stream or code
+  * It prints the passed string with a newline
+  * it also returns an integer value
+  * Return: 0
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
